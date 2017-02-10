5ed51a9a04b167c1c821532c3c3ced8b8fc2dbff
rightfold <rightfold@gmail.com>
2017-02-10 20:28:48 +0100

    First working example

--- examples/Math_main.py
+++ examples/Math_main.py
@@ -2,4 +2,5 @@ import Data_EuclideanRing
 import Math
 
 print('Expecting 8 and 2.')
-print(Math.divmod(Data_EuclideanRing.euclideanRingInt)(42)(5))
+pair = Math.divmod(Data_EuclideanRing.euclideanRingInt)(42)(5)
+print(pair.value0, pair.value1)
